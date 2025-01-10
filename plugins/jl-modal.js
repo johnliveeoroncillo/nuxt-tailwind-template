9fbb5df189784f69609b88edef2aba5ee608c2b9
JL <johnliveeoroncillo@gmail.com>
2025-01-10 21:35:13 +0800

    Update jl-modal.js

--- plugins/jl-modal.js
+++ plugins/jl-modal.js
@@ -9,53 +9,65 @@ export default (context, inject) => {
             const keys = Object.keys(modals).length;
             const count = keys + 1;
 
-            modals[count] = true;
-            const modalName = `${modal}${count > 1 ? `-${count}` : ''}`;
+            // const modalName = `${modal}${count > 1 ? `-${count}` : ''}`;
+            const modalName = options.component;
 
-            ModalComponent.modal = modalName;
-            ModalComponent.$modal = this;
-        
-            const newModal = new Vue({
-                ...ModalComponent,
-                template: ModalComponent.$el,
-                methods: {
-                    async callback() {
-                        if(typeof this.data.callback === 'function') {
-                            const options = this.data?.options ?? {};
-                            await this.data.callback(options);
-                            this.close();
-                        }
+            if (!modals[modalName]) {
+                ModalComponent.modal = modalName;
+                ModalComponent.$modal = this;
+            
+                const newModal = new Vue({
+                    ...context,
+                    ...ModalComponent,
+                    template: ModalComponent.$el,
+                    methods: {
+                        async callback() {
+                            if(typeof this.data.callback === 'function') {
+                                const options = this.data?.options ?? {};
+                                await this.data.callback(options);
+
+                                if (!this.data.preventClose) {
+                                    this.close();
+                                }
+                            }
+                        },
+                        showModal(data) {
+                            this.show = true;
+                            console.log(data, this.data, this.$options.modal);
+                            this.data = Object.assign({}, this.data, data);
+                            if (!data.layout) {
+                                this.data = Object.assign({}, this.data, { layout: 'default' });
+                            }
+                        },
+                        close() {
+                            this.show = false;
+                            this.data = {};
+                        },
                     },
-                    showModal(data) {
-                        this.show = true;
-                        console.log(data, this.data, this.$options.modal);
-                        this.data = Object.assign({}, this.data, data);
-                        if (!data.layout) {
-                            this.data = Object.assign({}, this.data, { layout: 'default' });
-                        }
+                    created() {
+                        console.log(modalName)
+                        this.$modal.on(`show_${modalName}`, this.showModal);
+                        this.$modal.on(`hide_${modalName}`, this.close);
                     },
-                    close() {
-                        this.show = false;
-                        this.data = {};
-                    },
-                },
-                created() {
-                    this.$modal.on(`show_${modalName}`, this.showModal);
-                    this.$modal.on(`hide_${modalName}`, this.close);
-                },
-                beforeDestroy() {
-                    this.$modal.off(`show_${this.$options.modal}`, this.showModal);
-                    this.$modal.off(`hide_${this.$options.modal}`, this.close);
-                }
-            });
+                    beforeDestroy() {
+                        this.$modal.off(`show_${modal}`, this.showModal);
+                        this.$modal.off(`hide_${modal}`, this.close);
+                        delete modals[modalName];
+                    }
+                });
+
+                modals[modalName] = newModal;
+            }
+            
             const element = document.createElement('div');
             element.setAttribute('id', modalName);
             document.querySelector('#__layout').appendChild(element);
-            newModal.$mount(`#${modalName}`)
+            modals[modalName].$mount(`#${modalName}`)
             
             bus.$emit(`show_${modalName}`, options);
         },
         hide(modal, options = {}) {
+            console.log(modal)
             bus.$emit(`hide_${modal}`, options)
         },
         on(event, callback) {
