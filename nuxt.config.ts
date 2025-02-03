9d1b42f770eb106ca9c96dba03cb795bbb2c94b7
John Livee Oroncillo <johnlivee.o@singlife.com>
2025-02-03 13:39:25 +0800

    off ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  ssr: false,
   shadcn: {
       /**
        * Prefix for all the imported component
