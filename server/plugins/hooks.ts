3d99339333488f8be5455bd306d629876b7aca42
John Livee Oroncillo <johnliveeoroncillo@gmail.com>
2025-02-25 17:49:02 +0800

    Removed 404 hook

--- server/plugins/hooks.ts
+++ server/plugins/hooks.ts
@@ -1,9 +1,11 @@
-export default defineNitroPlugin((nitroApp) => {
-    nitroApp.hooks.hook('afterResponse', (event) => {
-        if (!event.node.req.complete) {
-            event.node.res.statusCode = 404;
-            event.node.res.end('Not found');
-            return false;  // Prevent further processing of the request
-        }
-    })
-})
+// import type { H3Event } from 'h3'; // Import H3Event and HttpErrors
+
+// export default defineNitroPlugin((nitroApp) => {
+//     nitroApp.hooks.hook('beforeResponse', (event: H3Event) => {
+//         if (!event.node.req.complete) {
+//             event.node.res.statusCode = 404;
+//             event.node.res.end('Not found');
+//             return false;  // Prevent further processing of the request
+//         }
+//     })
+// })
